cc85bcfb12043701bbe2f220582a081bf962bf6d
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2022-12-16 12:46:57 +0300

    modified:   .eslintignore 	new file:   .eslintrc 	deleted:    .eslintrc.json 	new file:   .npmrc 	new file:   .nvmrc 	modified:   .parcelrc 	modified:   README.md 	deleted:    handlebars.config.js 	modified:   package-lock.json 	modified:   package.json 	new file:   src/core/Block.ts 	new file:   src/core/EventBus.ts 	new file:   src/core/index.ts 	new file:   src/core/registerComponent.ts 	new file:   src/core/renderDOM.ts 	renamed:    src/pages/index.hbs -> src/pages/index.html 	new file:   src/pages/login/index.html 	new file:   src/pages/login/index.ts 	modified:   tsconfig.json 	new file:   typings/app.d.ts 	new file:   typings/hbs.d.ts 	deleted:    utils/eventBus.ts

---
+++ src/core/EventBus.ts
@@ -0,0 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Listener<T extends unknown[] = any[]> = (...args: T) => void;
+
+// eslint-disable-next-line max-len, @typescript-eslint/no-explicit-any
+export default class EventBus<E extends string = string, M extends { [K in E]: unknown[] } = Record<E, any[]>> {
+  private listeners: { [key in E]?: Listener<M[E]>[] } = {};
+
+  on(event: E, callback: Listener<M[E]>) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event]?.push(callback);
+  }
+
+  off(event: E, callback: Listener<M[E]>) {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event]?.filter(
+      (listener) => listener !== callback,
+    );
+  }
+
+  emit(event: E, ...args: M[E]) {
+    if (!this.listeners[event]) {
+      return;
+      // throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event]?.forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
---
+++ src/pages/login/index.ts
@@ -0,0 +1,28 @@
+import template from 'bundle-text:./index.hbs';
+import { Block, renderDOM, registerComponent } from '../../core';
+
+import './index.css';
+
+require('babel-core/register');
+// import './app.css';
+
+// import Button from './components/button';
+// import Link from './components/link';
+// import Input from './components/input';
+// import Layout from './components/layout';
+
+// registerComponent(Button);
+// registerComponent(Link);
+// registerComponent(Input);
+// registerComponent(Layout);
+
+// TODO: // Добавить MyComponent
+class MyComponent extends Block {
+  render() {
+    return template;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderDOM(new MyComponent());
+});
