1ec5173878a3a35475c1a797bb876224539a998b
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2022-12-20 21:23:41 +0300

    modified:   src/pages/chat/index.hbs 	modified:   src/pages/chat/index.ts

--- src/pages/chat/index.ts
+++ src/pages/chat/index.ts
@@ -3,13 +3,54 @@ import { renderDOM, Block } from '../../core';
 
 import './index.css';
 
+const foto: URL = new URL(
+  '../../../assets/foto.svg',
+  import.meta.url,
+);
+const file: URL = new URL(
+  '../../../assets/file.svg',
+  import.meta.url,
+);
+const location: URL = new URL(
+  '../../../assets/location.svg',
+  import.meta.url,
+);
+const arrowLeft: URL = new URL(
+  '../../../assets/arrowLeft.svg',
+  import.meta.url,
+);
+const avatar: URL = new URL(
+  '../../../assets/avatar.svg',
+  import.meta.url,
+);
+const search: URL = new URL(
+  '../../../assets/search.svg',
+  import.meta.url,
+);
+const arrowRight: URL = new URL(
+  '../../../assets/arrowRight.svg',
+  import.meta.url,
+);
+
 class Index extends Block {
+  compile: any;
+
+  constructor({
+    foto, file, location, arrowLeft, avatar, search, arrowRight,
+  }: Record<string, URL>) {
+    super({
+      foto, file, location, arrowLeft, avatar, search, arrowRight,
+    });
+  }
+
   // eslint-disable-next-line class-methods-use-this
   render() {
-    return this.compile(template, { });
+    return this.compile(template, { ...this.props });
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  renderDOM('#app', new Index());
+  renderDOM('#app', new Index({
+    foto, file, location, arrowLeft, avatar, search, arrowRight,
+  }));
 });
