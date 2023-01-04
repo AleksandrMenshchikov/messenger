477acf3719e32f61cefc71e37abd95328f03e88d
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2023-01-04 10:04:29 +0300

    modified:   src/index.ts

---
+++ src/core/Store.ts
@@ -0,0 +1,25 @@
+import EventBus from './EventBus';
+import set from '../../utils/set';
+
+export enum StoreEvents {
+  Updated = 'updated',
+}
+
+// наследуем Store от EventBus, чтобы его методы были сразу доступны у экземпляра Store
+class Store extends EventBus {
+  private state: Record<string, unknown> = {};
+
+  public getState() {
+    return this.state;
+  }
+
+  public set(path: string, value: unknown) {
+    set(this.state, path, value);
+
+    // метод EventBus
+    this.emit(StoreEvents.Updated);
+  }
+}
+
+const store = new Store();
+export default store;
