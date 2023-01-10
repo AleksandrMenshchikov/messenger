b5dde40fbbe41c2d47265a8f9552c8da2e967753
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2023-01-10 20:03:01 +0300

    modified:   src/controllers/login-controller.ts 	modified:   src/controllers/signup-controller.ts

--- src/controllers/signup-controller.ts
+++ src/controllers/signup-controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import authApi from '../api/auth-api';
+import chatsApi from '../api/chats-api';
 import URLs from '../api/urls';
 import router from '../core/Router';
 import store from '../core/Store';
@@ -33,6 +34,25 @@ class SignupController {
               store.set('profileInputDisplayName.value', String(state.user.display_name));
               store.set('profileInputPhone.value', state.user.phone);
               store.set('profileTitle.content', String(state.user.display_name));
+              chatsApi.getChats().then((res) => {
+                if ((res as XMLHttpRequest).status === 200) {
+                  const arr: Record<string, unknown>[] = JSON
+                    .parse((res as XMLHttpRequest).response);
+                  const obj = arr.reduce((acc, item, index) => {
+                    acc[index] = item;
+                    if ((acc[index] as Record<string, unknown>).avatar) {
+                      (acc[index] as Record<string, unknown>).avatar = `${URLs.hostAvatar}${(acc[index] as Record<string, unknown>).avatar}`;
+                    } else {
+                      (acc[index] as Record<string, unknown>).avatar = 'https://www.lightsong.net/wp-content/uploads/2020/12/blank-profile-circle.png';
+                    }
+                    return acc;
+                  }, {});
+                  store.set('chats.data', null);
+                  store.set('chats.data', obj);
+                } else {
+                  console.log(JSON.parse((res as XMLHttpRequest).response.reason));
+                }
+              }).catch((err) => console.log(err));
               router.replace('/messenger');
             }
           }).catch((err) => {
