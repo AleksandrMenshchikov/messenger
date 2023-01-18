d8697c54e16c19b236c2074fc0d7f7b3944be6a3
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2023-01-18 12:50:20 +0300

    renamed:    app.ts -> app.js 	new file:   babel.config.js 	modified:   package-lock.json 	modified:   package.json 	renamed:    src/index.html -> public/index.html 	modified:   src/components/avatar/avatar.hbs 	modified:   tsconfig.json 	new file:   webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 4000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      { test: /\.hbs$/, loader: 'handlebars-loader' },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
