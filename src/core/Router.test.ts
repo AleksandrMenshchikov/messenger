abb52b8dbbdb9c3ee0d3811543deb9b9d785588c
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2023-01-27 18:05:20 +0300

    modified:   src/core/Router.test.ts

--- src/core/Router.test.ts
+++ src/core/Router.test.ts
@@ -4,7 +4,7 @@ describe('Router', () => {
   describe('router.go("/123")', () => {
     test('should change window.location.href from http://localhost/ to http://localhost/123', () => {
       router.go('/123');
-      expect(window.location.href).toBe('http://localhost/121');
+      expect(window.location.href).toBe('http://localhost/123');
     });
   });
 });
