6f5c8ff8d2ddd580012588d9b1cdb149bbba06ca
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2023-01-28 19:20:43 +0300

    modified:   src/core/Router.test.ts

--- src/core/Router.test.ts
+++ src/core/Router.test.ts
@@ -4,43 +4,30 @@ import loginPage from '../pages/login';
 import messengerPage from '../pages/messenger';
 import settingsPage from '../pages/settings';
 import signupPage from '../pages/signup';
+import Route from './Route';
 import router, { Router } from './Router';
 
 jest.mock('nanoid', () => ({ nanoid: () => Math.floor(Math.random() * (99000 - 10000) + 10000) }));
 
 describe('Router', () => {
-  describe('router.use("./", loginPage), router.use("./signup", signupPage), router.use("./settings", settingsPage), router.use("./messenger", messengerPage), router.use("./404", notFoundPage), router.use("./500", serverErrorPage)', () => {
-    test('should return Router and call method "start"', () => {
-      router.use('./', loginPage);
-      router.use('./signup', signupPage);
-      router.use('./settings', settingsPage);
-      router.use('./messenger', messengerPage);
-      router.use('./400', notFoundPage);
-      router.use('./500', serverErrorPage);
-      expect(Router).toStrictEqual(Router);
-      const callback = jest.fn();
-      router.start(callback());
-      expect(callback).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('router.go("/any-path")', () => {
-    test('should change window.location.href from http://localhost/ to http://localhost/any-path', () => {
-      router.go('/any-path');
-      expect(window.location.href).toStrictEqual('http://localhost/any-path');
-    });
-    test('should change window.history.length from 1 to 2', () => {
-      expect(window.history.length).toStrictEqual(2);
-    });
-  });
-
-  describe('router.replace("/another-path")', () => {
-    test('should change window.location.href from http://localhost/ to http://localhost/another-path', () => {
-      router.replace('/another-path');
-      expect(window.location.href).toStrictEqual('http://localhost/another-path');
-    });
-    test('do not should change window.history.length', () => {
-      expect(window.history.length).toStrictEqual(2);
-    });
+  test('should return Router and calls methods "start", "getRoute", "go", "replace"', () => {
+    router.use('./', loginPage);
+    router.use('./signup', signupPage);
+    router.use('./settings', settingsPage);
+    router.use('./messenger', messengerPage);
+    router.use('./400', notFoundPage);
+    router.use('./500', serverErrorPage);
+    expect(Router).toStrictEqual(Router);
+    const callback = jest.fn();
+    router.start(callback());
+    expect(callback).toHaveBeenCalledTimes(1);
+    router.getRoute('./');
+    expect(Route).toStrictEqual(Route);
+    router.go('/any-path');
+    expect(window.location.href).toStrictEqual('http://localhost/any-path');
+    expect(window.history.length).toStrictEqual(2);
+    router.replace('/another-path');
+    expect(window.location.href).toStrictEqual('http://localhost/another-path');
+    expect(window.history.length).toStrictEqual(2);
   });
 });
