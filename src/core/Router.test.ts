ad04fc1be63ce73c1b479926a73941f39b7dfa62
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2023-01-28 08:42:15 +0300

    modified:   src/core/Router.test.ts

--- src/core/Router.test.ts
+++ src/core/Router.test.ts
@@ -1,10 +1,23 @@
 import router from './Router';
 
 describe('Router', () => {
-  describe('router.go("/123")', () => {
-    test('should change window.location.href from http://localhost/ to http://localhost/123', () => {
-      router.go('/123');
-      expect(window.location.href).toBe('http://localhost/123');
+  describe('router.go("/any-path")', () => {
+    test('should change window.location.href from http://localhost/ to http://localhost/any-path', () => {
+      router.go('/any-path');
+      expect(window.location.href).toStrictEqual('http://localhost/any-path');
+    });
+    test('should change window.history.length from 1 to 2', () => {
+      expect(window.history.length).toStrictEqual(2);
+    });
+  });
+
+  describe('router.replace("/another-path")', () => {
+    test('should change window.location.href from http://localhost/ to http://localhost/another-path', () => {
+      router.replace('/another-path');
+      expect(window.location.href).toStrictEqual('http://localhost/another-path');
+    });
+    test('do not should change window.history.length', () => {
+      expect(window.history.length).toStrictEqual(2);
     });
   });
 });
