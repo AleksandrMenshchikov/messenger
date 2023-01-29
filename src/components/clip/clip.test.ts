9fc616095f43c7d92543c6605fb8365b4b184bc6
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2023-01-29 11:01:16 +0300

    new file:   src/components/clip/clip.test.ts 	new file:   src/components/dots/dots.test.ts 	new file:   src/components/empty-messages/empty-messages.test.ts

---
+++ src/components/clip/clip.test.ts
@@ -0,0 +1,20 @@
+import { Clip } from './clip';
+
+jest.mock('nanoid', () => ({ nanoid: () => Math.floor(Math.random() * (99000 - 10000) + 10000) }));
+
+describe('button', () => {
+  test('should work event on click ', () => {
+    const mock = jest.fn();
+    document.body.innerHTML = '<div id="app"></div>';
+    const element = new Clip({
+      events: {
+        click: mock(),
+      },
+    }).getContent();
+    const app = document.body.querySelector('#app') as HTMLDivElement;
+    app.appendChild(element);
+    const button = app.querySelector('button') as HTMLButtonElement;
+    button.click();
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+});
