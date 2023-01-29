4bbfe3213be67cc6603e770265de4e74e6731b8d
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2023-01-29 10:02:27 +0300

    new file:   src/components/button-add-user/button-add-user.test.ts 	new file:   src/components/button-avatar/button-avatar.test.ts

---
+++ src/components/button-avatar/button-avatar.test.ts
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { prettyDOM } from '@testing-library/dom';
+import { ButtonAvatar } from './button-avatar';
+
+jest.mock('nanoid', () => ({ nanoid: () => Math.floor(Math.random() * (99000 - 10000) + 10000) }));
+
+describe('button', () => {
+  test('should work event on click ', () => {
+    const mock = jest.fn();
+    document.body.innerHTML = '<div id="app"></div>';
+    const element = new ButtonAvatar({
+      avatar: 'https://www.lightsong.net/wp-content/uploads/2020/12/blank-profile-circle.png',
+      events: {
+        click: mock(),
+      },
+    }).getContent();
+    const app = document.body.querySelector('#app') as HTMLDivElement;
+    app.appendChild(element);
+    const button = app.querySelector('button') as HTMLButtonElement;
+    button.click();
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+});
