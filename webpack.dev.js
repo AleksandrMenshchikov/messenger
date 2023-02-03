07eb7325f4b73b976c9c2a11688307668a76e2b3
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2023-02-03 21:04:53 +0300

    modified:   webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     static: path.resolve(__dirname, './dist'),
     open: true,
     compress: true,
-    port: 4000,
+    port: 3000,
     historyApiFallback: true,
   },
 });
